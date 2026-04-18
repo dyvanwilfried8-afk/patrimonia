5971a95f37943bb0f7a1bc5dcd0229b983e266a9
dyvanwilfried8-afk <dyvanwilfried8@gmail.com>
2026-04-18 19:22:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ⚠️ Changer la version force le navigateur à vider l'ancien cache
-const CACHE = 'patrimonia-v7';
+const CACHE = 'patrimonia-v6';
 
 // Fichiers à mettre en cache au premier chargement
 const PRECACHE = [
@@ -7,7 +7,6 @@ const PRECACHE = [
   './index.html',
   './dashboard.html',
   './app.js',
-  './app.js?v=5',
 ];
 
 self.addEventListener('install', e => {
